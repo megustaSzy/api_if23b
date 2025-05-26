0980b5fcbac98aaf4b78312f56cd236831d09289
Raditya Ahmad <raditiaahmad50@gmail.com>
2025-05-26 20:36:23 +0700

    commit mobile

---
+++ connection.php
@@ -0,0 +1,9 @@
+<?php
+$SERVER = "localhost";
+$user = "root";
+$pass = "";
+$db = "db_if23b";
+
+$connect = mysqli_connect($SERVER, $user, $pass, $db);
+
+?>
---
+++ db_if23b.sql
@@ -0,0 +1,58 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Waktu pembuatan: 26 Bulan Mei 2025 pada 15.11
+-- Versi server: 10.4.32-MariaDB
+-- Versi PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_if23b`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktur dari tabel `tbl_mahasiswa`
+--
+
+CREATE TABLE `tbl_mahasiswa` (
+  `npm` char(10) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `prodi` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data untuk tabel `tbl_mahasiswa`
+--
+
+INSERT INTO `tbl_mahasiswa` (`npm`, `nama`, `prodi`) VALUES
+('23312006', 'Agung', 'Informatika'),
+('23312011', 'Bima', 'informatika'),
+('23312028', 'Radit', 'Informatika');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indeks untuk tabel `tbl_mahasiswa`
+--
+ALTER TABLE `tbl_mahasiswa`
+  ADD PRIMARY KEY (`npm`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
